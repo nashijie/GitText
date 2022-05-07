6e3237efd615891a71ab82199342d52b232e55a9
nashijie <nashijie@sinosoft.com.cn>
2022-05-07 11:31:50 +0800

    maste分支提交循环

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,5 +1,8 @@
 public class HelloWord {
     public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println("hihihihi");
+        }
         System.out.println("Hello Word");
     }
 }
