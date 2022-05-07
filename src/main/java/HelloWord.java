908fce8cf96bc9c6eb82ae42a6d43ce8c0dca1e1
nashijie <nashijie@sinosoft.com.cn>
2022-05-07 11:13:50 +0800

    beijing分支添加循环

--- src/main/java/HelloWord.java
+++ src/main/java/HelloWord.java
@@ -1,5 +1,8 @@
 public class HelloWord {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("你好");
+        }
         System.out.println("Hello Word");
     }
 }
